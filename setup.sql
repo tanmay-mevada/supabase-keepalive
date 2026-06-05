5f4366018fe52254155213428e75576184d1643b
Tanmay Mevada <tanmaymevada24@gmail.com>
2026-06-05 08:38:15 +0530

    Remove unnecessary comment from setup.sql

--- setup.sql
+++ setup.sql
@@ -1,5 +1,3 @@
--- Go to your Supabase project → SQL Editor → run this query
-
 -- Stores periodic keep-alive requests
 CREATE TABLE IF NOT EXISTS keep_alive (
   id SERIAL PRIMARY KEY,              -- Unique ping ID
