d2b54cffabd09577455eced7f139e14ffa919572
Tanmay Mevada <tanmaymevada24@gmail.com>
2026-06-05 09:03:34 +0530

    Disable row-level security for keep_alive table
    Added a command to disable row-level security for the keep_alive table.

--- setup.sql
+++ setup.sql
@@ -3,3 +3,4 @@ CREATE TABLE IF NOT EXISTS keep_alive (
   id SERIAL PRIMARY KEY,              -- Unique ping ID
   pinged_at TIMESTAMPTZ DEFAULT NOW() -- Timestamp of the ping
 );
+ALTER TABLE keep_alive DISABLE ROW LEVEL SECURITY; --Ensures RLS Policies dont stop insertion for this table
